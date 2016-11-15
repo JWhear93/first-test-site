6a224b8708e7071cb78de90cce7dbfdd67fa1c75
jwhear93 <jaweebelow420@gmail.com>
2016-11-15 16:07:30 +0000

    Add welcome message in js

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,8 @@
 var myHeading = document.querySelector('h1');
 myHeading.textContent = 'Hello world!';
+var myVariable = "Message"
+if (myVariable==='Message') {
+	alert('Welcome!');
+}	else {
+	alert('Welcome');
+}
